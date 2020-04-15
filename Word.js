18ad1c4592c1fc4baf5f8024030c9d748bce3ec6
Sean Baklini <sbaklini@unm.edu>
2020-04-14 22:06:57 -0600

    Word Class working

--- Word.js
+++ Word.js
@@ -1,1 +1,26 @@
-let Letter = require("word.js");
+let Letter = require('word.js');
+
+function Word(word) {
+    this.letterArray = function () {
+        let splitWord = word.split('');
+        const letterMap = splitWord.map(letter => {
+            return new Letter(letter)
+        });
+        return letterMap;
+    }
+
+    this.printWord = function (userGuess) {
+
+        const checkMap = this.letterArray.map(letter => {
+            return letter.display(userGuess)
+        });
+
+        return checkMap.join(' ');
+    }
+
+    this.checkLetter = function (userGuess) {
+        this.letterArray.forEach(letter => {
+            letter.check(userGuess);
+        })
+    }
+}
